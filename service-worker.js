d62d88f88beb4db50a0d8c0b160631805067e88a
rugbymanthonon-eng <rugbymanthonon@gmail.com>
2026-08-24 08:27:11 +0200

    Actualiser le cache pour les libellés de mois

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,11 @@
-const CACHE = "capi-v7";
+const CACHE = "capi-v8";
 const ASSETS = [
   "./",
   "./index.html",
   "./styles.css",
   "./app-fixed.js?v=5",
   "./stats-enhanced.js?v=5",
-  "./chart-label-fix.js?v=7",
+  "./chart-label-fix.js?v=8",
   "./stats-theme.js?v=6",
   "./stats-core-v6.js?v=6",
   "./db.js",
